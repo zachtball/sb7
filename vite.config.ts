3319c54413d39085907d1088d70fd0d1cf725743
Zach Ball <zball@freedomdebtrelief.com>
2023-03-02 12:02:18 -0800

    fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      components: './src/components',
+      components: path.resolve(__dirname, './src/components'),
     },
   },
 });
